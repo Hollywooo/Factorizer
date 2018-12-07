0535acbe7376d0e5493c1f92adf5a9aca964a7f1
Hollywooo <rodneystubbs@gmail.com>
2018-12-07 09:35:08 -0600

    20181207 Initial commit. Prime factorization of Int32.

---
+++ Factorizer/Factors.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorizer
+{
+    class Factors
+    {
+        int inputInt = new int();
+        List<int> factorList = new List<int>();
+
+        public static Factors Factorize(int input)
+        {
+            Factors factorThis = new Factors();
+            factorThis.inputInt = input;
+
+            int currentValue = factorThis.inputInt;
+            double value = factorThis.inputInt;            
+            int sqrMark = Convert.ToInt32(Math.Round(Math.Sqrt(value), 0, MidpointRounding.AwayFromZero));
+            int iter = 2;
+
+            while (iter <= sqrMark)
+            {
+                if (currentValue % iter == 0)
+                {
+                    currentValue = currentValue / iter;
+                    factorThis.factorList.Add(iter);
+                }
+
+                else
+                {
+                    iter++;
+                }
+            }
+
+            if (currentValue > iter)
+            {
+                factorThis.factorList.Add(currentValue);
+            }
+            else { }
+
+            return factorThis;
+        }
+
+        public static void FactorOutput(Factors factoredInput)
+        {
+            Factors solvedFactor = factoredInput;
+            string factorListString = null;
+            factorListString = "The prime factorization of " + solvedFactor.inputInt + " is: [";
+
+            foreach (var item in solvedFactor.factorList)
+            {
+                factorListString = factorListString + item.ToString() + ", ";
+            }
+
+            factorListString = factorListString + "].";
+
+            Console.WriteLine(factorListString);
+        }
+    }
+}
---
+++ Factorizer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorizer
+{    
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string userDirection = "yes";
+            while (userDirection != "no")
+            {
+                Console.WriteLine("Prime Factorization Program: please enter an integer.  ");
+                int.TryParse(Console.ReadLine().ToString(), out int userInput);
+
+                Factors.FactorOutput(Factors.Factorize(userInput));
+                Console.ReadKey();
+                Console.WriteLine("\r\n" + "Perform another factorization? yes/no" + "\r\n");
+                userDirection = Console.ReadLine().ToString();
+            }
+
+            Console.WriteLine("\r\n" + "Goodbye!");
+            Console.ReadKey();
+            
+            
+
+        }
+    }
+}
